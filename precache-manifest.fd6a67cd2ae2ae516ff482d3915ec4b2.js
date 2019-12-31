278d6baad7b02b4061ca62fb090332b65f5fbfcc
Roy <cjh19891018@gmail.com>
2019-12-31 13:15:40 +0800

    Updates

---
+++ precache-manifest.fd6a67cd2ae2ae516ff482d3915ec4b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451d1cf578e64d9e7098bfd13e51e026",
+    "url": "/uploadimg-ts/index.html"
+  },
+  {
+    "revision": "3a852a3b5008791cb6c3",
+    "url": "/uploadimg-ts/static/css/main.0931c225.chunk.css"
+  },
+  {
+    "revision": "3628f5cf0d1eebe34036",
+    "url": "/uploadimg-ts/static/js/2.69b81333.chunk.js"
+  },
+  {
+    "revision": "6207cc49c334a88845df4e47971d5513",
+    "url": "/uploadimg-ts/static/js/2.69b81333.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a852a3b5008791cb6c3",
+    "url": "/uploadimg-ts/static/js/main.3aff01f4.chunk.js"
+  },
+  {
+    "revision": "0da7e7a03f6055fccaa4",
+    "url": "/uploadimg-ts/static/js/runtime-main.9cda8963.js"
+  }
+]);
